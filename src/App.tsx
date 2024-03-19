c0cd27dccd4abc14657a5db3dc39c3bfeb030b23
Deni Junior <denidiasjr@gmail.com>
2024-03-19 21:30:00 +0000

    feat: Add under construction message

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ImageConstruction from './images/under_construction.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDev, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        denidiasjr.github.io
+      </header>
+      <section>
+        <img alt="Under construction sign" src={ImageConstruction} />
+        Oops! My personal web page in under development.<br/>
+        See you soon! 😄
+      </section>
+      <footer>
+        <a href="https://github.com/denidiasjr" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href="https://linkedin.com/in/denidiasjr" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+        <a href="https://dev.to/denidiasjr" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faDev} />
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
